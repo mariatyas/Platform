34f6b1abe10d2ce250009975626de3bfb8524944
mariatyas <mariatyas2003@gmail.com>
2023-05-15 18:47:24 +0700

    deleted:    TugasPHP/koneksi.php 	new file:   TugasPHP/regis.php 	new file:   TugasPHP/registrasi.php

---
+++ TugasPHP/registrasi.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Registrasi</title>
+</head>
+<body>
+
+<h1>Halaman Registrasi</h1>
+
+<form action="" method="post">
+
+    <ul>
+        <label for="username">username :</label>
+        <input type="text" name="username" id="username">
+
+        <label for="password">password :</label>
+        <input type="password" name="password" id="password">
+
+        <label for="password2">konfirmasi password :</label>
+        <input type="password" name="password2" id="password2">
+
+        <button type="submit" name="register">Register</button>
+    </ul>
+
+</form>
+
+</body>
+</html>
